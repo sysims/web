4d74334c93dc98c90f5eedf091fb0c0d839aefd1
Hilal <affashilal@gmail.com>
2026-06-23 16:04:46 +0700

    update progress

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import '@tabler/core/dist/css/tabler.min.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+//import '@tabler/core/dist/css/tabler.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
